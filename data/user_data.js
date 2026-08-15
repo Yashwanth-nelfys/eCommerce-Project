d6670a0f3a06aa5ef3d2284fd78916437ad4464d
T.Yashwanth <tyashwanthkumar48@gmail.com>
2026-08-15 23:00:36 +0530

    new pro

---
+++ data/user_data.js
@@ -0,0 +1,57 @@
+export let users =
+  [{
+    "id": 123,
+    "userId": 123,
+    "userName": "Nandhini",
+    "profileImage": "",
+    "isAdmin": false,
+    "email": "nandhini321@gmail.com",
+    "phoneNo": "8976543454",
+    "addresses": "Kukatpally, Hyderabad, Telangana",
+    "region": "Hyderabad"
+  },
+  {
+    "id": 236,
+    "userId": 236,
+    "userName": "Meghana",
+    "profileImage": "",
+    "isAdmin": false,
+    "email": "meghana451@gmail.com",
+    "phoneNo": "9667654454",
+    "addresses": "Hanamkonda, Warangal, Telangana",
+    "region": "Warangal"
+  },
+  {
+    "id": 767,
+    "userId": 767,
+    "userName": "Niharika",
+    "profileImage": "",
+    "isAdmin": false,
+    "email": "niharika873@gmail.com",
+    "phoneNo": "7859893582",
+    "addresses": "Dichpally, Nizamabad, Telangana",
+    "region": "Nizamabad"
+  },
+  {
+    "id": 999,
+    "userId": 999,
+    "userName": "Keerthana",
+    "profileImage": "",
+    "isAdmin": false,
+    "email": "keerthana21@gmail.com",
+    "phoneNo": "6976543999",
+    "addresses": "Kothagudem, Bhadradri Kothagudem, Telangana",
+    "region": "Bhadradri Kothagudem"
+  },
+  {
+    "id": 907,
+    "userId": 907,
+    "userName": "Kalpana",
+    "profileImage": "",
+    "isAdmin": false,
+    "email": "kalpana112@gmail.com",
+    "phoneNo": "8776547299",
+    "addresses": "Mallapur, Karimnagar, Telangana",
+    "region": "Karimnagar"
+  }
+  ]
